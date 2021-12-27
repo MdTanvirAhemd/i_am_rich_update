38bc94b19847b7850b3a6969c5ee989f70107a97
Overlord <tanvir1005@student.sust.edu>
2021-12-27 23:53:07 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I am Rich.'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Image(image: NetworkImage('https://www.hdnicewallpapers.com/Walls/Big/Rainbow/Rainbow_on_Mountain_HD_Image.jpg'),),
+    ),
+  ),
+  );
+}
